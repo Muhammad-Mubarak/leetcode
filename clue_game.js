9c604abda60150c10efff7450f69a6f5d8608b0b
Muhammad-Mubarak <muhammadmubarak1998@gmail.com>
2020-07-27 16:04:14 +0500

    checking git is working or not

---
+++ clue_game.js
@@ -0,0 +1,16 @@
+var game={};
+game.murderer = "??";
+
+game['weapons'] = [
+	{type: 'lasers', location: 'lab'},
+	{type: 'angry cats' ...},
+	{... 'dish soap' ...}
+];
+
+
+game.name = [];
+
+game.name[0] = 'Miss scarlet';
+
+game.name.push('Mr.Green')
+// ['miss Scarlet', 'Mr. Green']
